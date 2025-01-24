2a585f126af45d558d6b60e48e77a3a798b9e362
Simon Baeriswyl <sim149.sb@gmail.com>
2025-01-24 16:22:53 +0100

    Updated Discord username to kitsurashii

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Copy Discord username to clipboard
 document.getElementById('discord-copy').addEventListener('click', function() {
-  navigator.clipboard.writeText('subaru_rexie');
+  navigator.clipboard.writeText('kitsurashii');
   alert('Discord username copied to clipboard!');
 });
 
